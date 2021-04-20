4a5f213e6c456c46a1da6d76f155e99b7b933160
Sylvia D. Antonsdottir <sylvia@donchev.is>
2021-04-20 09:30:06 +0200

    Add PDF processing

---
+++ ts/src/lib/pdf.ts
@@ -0,0 +1,29 @@
+import * as fileType from 'file-type'
+import * as PDFJS from 'pdfjs-dist/es5/build/pdf'
+// import * as PDFJS from 'pdfjs-dist'
+
+const isPDFfle = async (buf: Buffer) => {
+  const fileExt = await fileType.fromBuffer(buf)
+  if (!fileExt?.mime.toLocaleLowerCase().includes('pdf')) {
+    return false
+  }
+
+  return true
+}
+
+async function* getText(data: Buffer) {
+  if (!isPDFfle) throw new Error('Not a PDF file')
+
+  const pdf = await PDFJS.getDocument({ data }).promise
+
+  const maxPages = pdf.numPages
+
+  for (let i = 1; i <= maxPages; i++) {
+    const page = await pdf.getPage(i)
+    const text = await page.getTextContent()
+
+    yield { page: i, content: text.items.map((item: any) => item.str).join(' ') }
+  }
+}
+
+export { getText }
