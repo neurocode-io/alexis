840568b8c3fc9059e8895fb06e0ee9803c2e8538
Ziad Shaban <shaban_ziad@hotmail.com>
2021-04-24 13:09:50 +0200

    safer method for keeping track of last ID

--- server/consumer.ts
+++ server/consumer.ts
@@ -1,22 +1,40 @@
+import fs from 'fs'
 import Redis from 'ioredis'
+
+import log from './lib/log'
 
 const redis = new Redis()
 
-export const startConsumer = async () => {
-  let lastID = '$'
+export const startConsumer = () => {
+  let lastID : string
   let counter = 0
 
-  const start = async () => {
-    const result = await redis.xread('BLOCK', '0', 'COUNT', '1', 'STREAMS', 'pdfStream', lastID)
+  const start = () => {
+    fs.readFile(`${__dirname}/lastID`, 'utf8', async (err, data) => {
+        if (err || data.length === 0) {
+            lastID = '$'
+        } else {
+            lastID = data
+        }
+        
+        const result = await redis.xread('BLOCK', '0', 'COUNT', '1', 'STREAMS', 'pdfStream', lastID)
 
-    lastID = result[0]![1]![0]![0]
-    const content = result[0]![1]![0]![1]![3]
-    console.log(lastID)
-    console.log(content)
-    console.log(counter++)
-
-    setTimeout(start, 0)
+        lastID = result[0]![1]![0]![0]
+        fs.writeFile(`${__dirname}/lastID`, lastID, (err) => {
+            if (err) {
+                log.error(err)
+            }
+    
+            const content = result[0]![1]![0]![1]![3]
+    
+            console.log(lastID)
+            console.log(content)
+            console.log(counter++)
+        
+            setTimeout(start, 0)
+        })
+    })
   }
 
-  setTimeout(start, 0)
+  start();
 }
