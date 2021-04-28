85fb2536fe483b977d7dfccae6b92b1eeaca91a0
Ziad Shaban <shaban_ziad@hotmail.com>
2021-04-29 00:00:51 +0200

    changed to async file reading

--- server/lib/pdf.test.ts
+++ server/lib/pdf.test.ts
@@ -4,20 +4,22 @@ import { getText } from './pdf'
 
 describe('pdf', () => {
   describe('getText', () => {
-    it('should extract text from PDF', async () => {
-      const pdfContent = fs.readFileSync('./test-files/EffectiveAggregateDesign.pdf')
-
-      for await (const obj of getText(pdfContent)) {
-        expect(obj).toHaveProperty('page')
-        expect(obj).toHaveProperty('content')
-      }
+    it('should extract text from PDF', () => {
+      fs.readFile(`${__dirname}./test-files/EffectiveAggregateDesign.pdf`, async (err, data) => {
+        expect(err).toBeNull()
+        for await (const obj of getText(data)) {
+          expect(obj).toHaveProperty('page')
+          expect(obj).toHaveProperty('content')
+        }
+      })
     })
+
     it('should not extract from invalid PDF', () => {
       const pdfContent = Buffer.from('hello world')
 
       getText(pdfContent)
         .next()
-        .catch((e: Error) => expect(e.message).toEqual('Not a PDF file'))
+        .catch((e) => expect(e).toMatch('Not a PDF file'))
     })
   })
 })
