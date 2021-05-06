56bd5460cb33dc88201a13cf840cb6719fa9d580
Bobby Donchev <contact@donchev.is>
2021-05-06 09:27:52 +0200

    remove unused files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,10 @@
 import React from 'react'
-import logo from './logo.svg'
 import Registration from './components/Registration'
-import './App.css'
 
 function App() {
   return (
     <div className="App">
       <Registration />
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload!!
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header> */}
     </div>
   )
 }
