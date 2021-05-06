da97432d26ea511c8922f032e95f2a03fb545fff
Bobby Donchev <contact@donchev.is>
2021-05-06 09:24:24 +0200

    switch backend port

--- server/config/index.ts
+++ server/config/index.ts
@@ -1,45 +1,20 @@
 import * as env from 'env-var'
 
 const redisConfig = {
-  host: env
-    .get('REDIS_HOST')
-    .required()
-    .asString(),
-  port: env
-    .get('REDIS_PORT')
-    .required()
-    .asInt(),
-  password: env
-    .get('REDIS_PASSWORD')
-    .required()
-    .asString(),
-  namespace: env
-    .get('REDIS_KEY_PREFIX')
-    .default('ax')
-    .asString()
+  host: env.get('REDIS_HOST').required().asString(),
+  port: env.get('REDIS_PORT').required().asInt(),
+  password: env.get('REDIS_PASSWORD').required().asString(),
+  namespace: env.get('REDIS_KEY_PREFIX').default('ax').asString()
 }
 
 const serverConfig = {
-  port: env
-    .get('SERVER_PORT')
-    .default(3000)
-    .asPortNumber(),
-  logLevel: env
-    .get('SERVER_LOG_LEVEL')
-    .required()
-    .asString()
-    .toLowerCase(),
+  port: env.get('SERVER_PORT').default(5000).asPortNumber(),
+  logLevel: env.get('SERVER_LOG_LEVEL').required().asString().toLowerCase(),
   appName: 'alexis',
   pdfStream: 'pdf-processing',
-  sessionSecret: env
-    .get('SERVER_SESSION_SECRET')
-    .required()
-    .asString(),
+  sessionSecret: env.get('SERVER_SESSION_SECRET').required().asString(),
   maxPDFSize: env.get('SERVER_MAX_PDF_SIZE').asIntPositive(),
-  uploadDestionation: env
-    .get('SERVER_UPLOAD_DIR')
-    .default('uploads/')
-    .asString()
+  uploadDestionation: env.get('SERVER_UPLOAD_DIR').default('uploads/').asString()
 }
 
 export { redisConfig, serverConfig }
